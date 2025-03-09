2392a5d333ec55804ee987e86d828e28124976ef
SoXX <soxx@anthrove.art>
2025-03-09 21:38:03 +0100

    feat(cdn): integrate local storage provider for file retrieval
    Refactor cdn endpoint to use `ServeResource` method for serving files from local storage provider. Move logic for finding providers and sanitizing file paths to a new function in the `logic` package. Handles errors and improves code modularity.

--- internal/api/cdn.go
+++ internal/api/cdn.go
@@ -1,28 +1,8 @@
-/*
- * Copyright (C) 2025 Anthrove
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package api
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/anthrove/identity/pkg/object"
 	"github.com/gin-gonic/gin"
-	"path/filepath"
-	"strings"
+	"net/http"
 )
 
 // @Summary	Get an file
@@ -35,25 +15,11 @@ func (ir IdentityRoutes) cdnGetFile(c *gin.Context) {
 	tenantID := c.Param("tenant_id")
 	filePath := c.Param("file_path")
 
-	providers, err := ir.service.FindProviders(c, tenantID, object.Pagination{})
+	localFilePath, err := ir.service.ServeResource(c, tenantID, filePath)
 	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
 		return
 	}
-	sanitizedFilePath := strings.TrimPrefix(filepath.Clean(filePath), "/")
 
-	for _, provider := range providers {
-		if provider.Category == "storage" && provider.ProviderType == "local" {
-			var parameters map[string]string
-			if err := json.Unmarshal(provider.Parameter, &parameters); err != nil {
-				return
-			}
-
-			sanitizedBasePath := strings.TrimPrefix(filepath.Clean(parameters["base_path"]), "/")
-			localFilePath := fmt.Sprintf("local_storage_provider/%s/%s/%s", tenantID, sanitizedBasePath, sanitizedFilePath)
-
-			c.File(localFilePath)
-			return
-		}
-	}
-	return
+	c.File(localFilePath)
 }
---
+++ pkg/logic/cdn.go
@@ -0,0 +1,53 @@
+package logic
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/anthrove/identity/pkg/object"
+	"path/filepath"
+	"strings"
+)
+
+// ServeResource retrieves a specific resource within a specified tenant.
+//
+// Parameters:
+//   - ctx: context for managing request-scoped values, cancelation, and deadlines.
+//   - tenantID: unique identifier of the tenant to which the resource belongs.
+//   - filePath: unique identifier of the resource to be retrieved.
+//
+// Returns:
+//   - The local resource path.
+//   - Error if there is any issue during retrieval.
+func (is IdentityService) ServeResource(ctx context.Context, tenantID string, resourcePath string) (string, error) {
+	providers, err := is.FindProviders(ctx, tenantID, object.Pagination{})
+	if err != nil {
+		return "", err
+	}
+	sanitizedFilePath := sanitizeFilePath(resourcePath)
+
+	for _, provider := range providers {
+		if provider.Category == "storage" && provider.ProviderType == "local" {
+			parameters, err := unmarshalProviderParameters(provider)
+			if err != nil {
+				return "", err
+			}
+
+			sanitizedBasePath := sanitizeFilePath(parameters["base_path"])
+			localFilePath := fmt.Sprintf("local_storage_provider/%s/%s/%s", tenantID, sanitizedBasePath, sanitizedFilePath)
+
+			return localFilePath, nil
+		}
+	}
+	return "", fmt.Errorf("no suitable provider found")
+}
+
+func sanitizeFilePath(path string) string {
+	return strings.TrimPrefix(filepath.Clean(path), "/")
+}
+
+func unmarshalProviderParameters(provider object.Provider) (map[string]string, error) {
+	var parameters map[string]string
+	err := json.Unmarshal(provider.Parameter, &parameters)
+	return parameters, err
+}
