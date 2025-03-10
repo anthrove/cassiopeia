9f859ee8e9539cd67995c599fe21fed5424557b6
SoXX <soxx@anthrove.art>
2025-03-10 16:03:38 +0100

    feat(api): implement resource routes and error handling
    - Add error handling for findResource and findResources functions.
    - Implement functionality to retrieve resource by tenant ID and resource ID.
    - Retrieve existing groups with pagination support.
    - Update Swagger comments for accurate API documentation.

--- internal/api/resource.go
+++ internal/api/resource.go
@@ -17,6 +17,7 @@
 package api
 
 import (
+	"errors"
 	"github.com/anthrove/identity/pkg/object"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -105,26 +106,63 @@ func (ir IdentityRoutes) killResource(c *gin.Context) {
 // @Accept		json
 // @Param		tenant_id	path	string	true	"Tenant ID"
 // @Param		resource_id	query	string	true	"Resource ID"
+// @Success	200			{object}	HttpResponse{data=object.Resource{}}	"Resource"
+// @Failure	400			{object}	HttpResponse{data=nil}					"Bad Request"
 // @Produce	json
 // @Router		/api/v1/tenant/{tenant_id}/resource/{resource_id} [get]
 func (ir IdentityRoutes) findResource(c *gin.Context) {
-	// tenantID := c.Param("tenant_id")
-	// resourceID := c.Query("resource_id")
+	tenantID := c.Param("tenant_id")
+	resourceID := c.Param("resource_id")
 
-	// TODO: Implement Me
-	c.JSON(http.StatusForbidden, "Not Implemented yet")
+	resource, err := ir.service.FindResource(c, tenantID, resourceID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, HttpResponse{
+			Error: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, HttpResponse{
+		Data: resource,
+	})
 }
 
-// @Summary	Lists all resource
-// @Tags		Resource API
+// @Summary	Get existing Groups
+// @Tags		Group API
 // @Accept		json
-// @Param		tenant_id	path	string	true	"Tenant ID"
 // @Produce	json
-// @Router		/api/v1/tenant/{tenant_id}/resource [get]
+// @Param		page		query		string								false	"Page"
+// @Param		page_limit	query		string								false	"Page Limit"
+// @Param		tenant_id	path		string								true	"Tenant ID"
+// @Success	200			{object}	HttpResponse{data=[]object.Resource{}}	"Resources"
+// @Failure	400			{object}	HttpResponse{data=nil}				"Bad Request"
+// @Router		/api/v1/tenant/{tenant_id}/group [get]
 func (ir IdentityRoutes) findResources(c *gin.Context) {
-	// tenantID := c.Param("tenant_id")
-	// resourceID := c.Query("resource_id")
+	tenantID := c.Param("tenant_id")
 
-	// TODO: Implement Me
-	c.JSON(http.StatusForbidden, "Not Implemented yet")
+	pagination, ok := c.Get("pagination")
+	if !ok {
+		c.JSON(http.StatusInternalServerError, HttpResponse{
+			Error: "pagination parameter is missing",
+		})
+		return
+	}
+
+	paginationObj, ok := pagination.(object.Pagination)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, errors.New("pagination parameter cant be converted to object.Pagination"))
+		return
+	}
+
+	groups, err := ir.service.FindResources(c, tenantID, paginationObj)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, HttpResponse{
+			Error: err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, HttpResponse{
+		Data: groups,
+	})
 }
