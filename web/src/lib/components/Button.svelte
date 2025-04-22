e15c0e04f3b566328a0cc841dbfffee9ce07e58c
Stevetec-exe <39212124+Stevetec-exe@users.noreply.github.com>
2025-04-22 16:25:08 +0200

    fixed variant typing on buttons

--- web/src/lib/components/Button.svelte
+++ web/src/lib/components/Button.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
     import Link from "./link.svelte";
 
-    type ButtonVariant = 'primary' | 'danger'
+    type ButtonVariant = 'primary' | 'secondary' | 'danger'
     let {
         children,
         onclick = (e:PointerEvent) => {},
         href = "",
         class:classList = "",
-        variant = <ButtonVariant>"primary",
+        variant = 'primary' as ButtonVariant,
         type = 'button'
     } = $props();
 
     const VARIANTS = <{[key in ButtonVariant]:string}>{
         'primary':'bg-primary-500 text-white hover:bg-transparent border-[1px] border-primary-500 hover:text-primary-500',
-        'danger':'bg-rose-600 text-white hover:font-bold hover:bg-transparent border-[1px] border-rose-600 hover:text-rose-600'
+        'danger':'bg-rose-600 text-white hover:font-bold hover:bg-transparent border-[1px] border-rose-600 hover:text-rose-600',
+        'secondary':'border-[1px] border-zinc-500 text-black hover:bg-zinc-500 hover:text-white'
     }
 </script>
 
